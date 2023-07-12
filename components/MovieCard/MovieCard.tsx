7e1187e764eadba2f2fdf9e17975229d694402cb
Antonio Hauren <antoniohauren@protonmail.com>
2023-07-12 11:53:32 -0300

    feat(movie-card): add details button

--- components/MovieCard/MovieCard.tsx
+++ components/MovieCard/MovieCard.tsx
@@ -2,12 +2,16 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { BiChevronDown } from "react-icons/bi";
 import { BsPlayFill } from "react-icons/bs";
 import { MovieCardProps } from ".";
 import { FavoriteButton } from "../FavoriteButton";
+import { useMoreInfoModal } from "../MoreInfoModal";
 
 export function MovieCard({ data }: MovieCardProps) {
   const router = useRouter();
+
+  const { open } = useMoreInfoModal();
 
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
@@ -40,6 +44,16 @@ export function MovieCard({ data }: MovieCardProps) {
             </div>
 
             <FavoriteButton movieId={data.id} />
+
+            <div
+              onClick={() => open(data.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-s-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+            >
+              <BiChevronDown
+                className="text-white group-hover/item:text-neutral-300"
+                size={30}
+              />
+            </div>
           </div>
 
           <p className="text-green-400 font-semibold mt-4">
